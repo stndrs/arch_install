834f6d7beea325026f6b0fcdcd3edf1fa6178624
Stephen Anders <me@stndrs.ca>
2019-08-05 00:09:34 -0400

    Enable nordvpn daemon

--- user_setup.sh
+++ user_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Install aur packages
-yay -S ttf-dejavu rbenv ruby-build nvm authenticator
+yay -S ttf-dejavu rbenv ruby-build nvm authenticator nordvpn-bin
+sudo systemctl enable --now nordvpnd
 
 # Configure bash for nvm and rbenv
 echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
